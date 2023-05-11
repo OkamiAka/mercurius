619a760c8c163352ba344831c9a2d061f9ada9ed
Alex-5110 <alexpom@live.fr>
2023-05-11 16:51:30 +0200

    add logo in Header

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,15 @@
-import React, { useCallback, useState } from "react";
+import React from "react";
 import { Route, Routes } from "react-router-dom";
 import "react-tooltip/dist/react-tooltip.css";
 import "./App.scss";
+import Header from "./component/Header";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Routes>
-        <Route path="/" element={"la"} />
+        <Route path="/" element="la" />
       </Routes>
     </div>
   );
