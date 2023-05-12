73b5f2f44159db1afb24f44685080a9ee756109c
MOHAMED MAACH <maach.med@yahoo.com>
2023-05-12 09:45:32 +0200

    add more countries to code

--- frontend/src/components/Country.jsx
+++ frontend/src/components/Country.jsx
@@ -6,14 +6,57 @@ function CountryDetails({ country }) {
   const [countryDetails, setCountryDetails] = useState({});
   const [loading, setLoading] = useState(true);
   // fonction pour convertir le nom du pays en français en un code de pays à deux lettres
-
   function convertCountryNameToCode(frenchName) {
     const countriesCode = {
-      France: "FR",
-      Maroc: "MA",
+      "Maroc": "MA",
+      "Turquie": "TR",
+      "Thaïlande": "TH",
+      "Indonésie": "ID",
+      "Tunisie": "TN",
+      "Espagne": "ES",
+      "Grèce": "GR",
+      "Italie": "IT",
+      "Portugal": "PT",
+      "Mexique": "MX",
+      "Japon": "JP",
+      "Maldives": "MV",
+      "Nouvelle-Zélande": "NZ",
+      "Australie": "AU",
+      "République Dominicaine": "DO",
+      "France": "FR",
+      "Canada": "CA",
+      "États-Unis": "US",
+      "Norvège": "NO",
+      "Suède": "SE",
+      "Finlande": "FI",
+      "Islande": "IS",
+      "Népal": "NP",
+      "Pérou": "PE",
+      "Afrique du Sud": "ZA",
+      "Costa Rica": "CR",
+      "Inde": "IN",
+      "Égypte": "EG",
+      "Seychelles": "SC",
+      "Bahamas": "BS",
+      "Singapour": "SG",
+      "Suisse": "CH",
+      "Hong Kong": "HK",
+      "Albanie": "AL",
+      "Belgique": "BE",
+      "Corée du Sud": "KR",
+      "Lithuanie": "LT",
+      "Koweït": "KW",
+      "Kazakhstan": "KZ",
+      "Bahreïn": "BH",
+      "Estonie": "EE",
+      "Iran": "IR",
+      "Géorgie": "GE",
+      "Philippines": "PH",
     };
+  
     return countriesCode[frenchName];
   }
+  
   useEffect(() => {
     const countryCode = convertCountryNameToCode(country);
     // Récupérer les données du pays depuis l'API
